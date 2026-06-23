1c3dc778ed2340472bb0894b097f7644edb5c876
2254996 <2254996@gmail.com>
2026-06-23 15:10:46 +0400

    23June bot.py

--- bot.py
+++ bot.py
@@ -7,42 +7,134 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 KEYWORDS = [
-    "event", "events", "festival", "ceremony", "opening ceremony",
-    "national day", "exhibition", "museum", "visitor center",
-    "experience center", "immersive", "interactive", "multimedia",
-    "projection", "projection mapping", "audio visual", "av",
-    "led", "digital experience", "content production", "creative",
-    "media production", "show production", "pavilion", "hologram"
+    "event",
+    "events",
+    "festival",
+    "ceremony",
+    "opening ceremony",
+    "closing ceremony",
+    "inauguration",
+    "launch event",
+
+    "national day",
+    "uae national day",
+    "eid al etihad",
+    "union day",
+
+    "museum",
+    "visitor center",
+    "visitor centre",
+    "heritage",
+    "cultural",
+
+    "immersive",
+    "interactive",
+    "experience",
+    "experience center",
+
+    "multimedia",
+    "projection",
+    "projection mapping",
+    "3d mapping",
+
+    "audio visual",
+    "av",
+
+    "led",
+    "led screen",
+    "digital screen",
+    "digital signage",
+
+    "content production",
+    "show production",
+
+    "creative",
+    "creative services",
+
+    "animation",
+    "cg",
+    "hologram",
+
+    "exhibition",
+    "expo",
+    "pavilion",
+
+    "storytelling",
+    "cultural event",
+    "cultural festival",
+
+    "innovation",
+    "future",
+
+    "rfp",
+    "rfq",
+    "tender",
+    "procurement",
+    "bid",
+    "opportunity"
+]
 ]
 
 SOURCES = [
-    {
+      {
         "name": "Dubai eSupply",
-        "url": "https://esupply.dubai.gov.ae/esop/guest/go/opportunity/current?locale=en"
+        "url": "https://esupply.dubai.gov.ae"
     },
     {
-        "name": "Dubai Municipality",
-        "url": "https://www.dm.gov.ae/municipality-business/tenders-biddings/"
+        "name": "Abu Dhabi Government Procurement",
+        "url": "https://www.adgpg.gov.ae"
     },
     {
-        "name": "Expo City Dubai Suppliers",
-        "url": "https://www.expocitydubai.com/en/suppliers/"
+        "name": "Federal Procurement UAE",
+        "url": "https://procurement.gov.ae"
     },
     {
-        "name": "DEWA Tenders",
-        "url": "https://www.dewa.gov.ae/en/about-us/strategy-excellence/tenders"
+        "name": "Expo City Dubai",
+        "url": "https://www.expocitydubai.com"
     },
     {
-        "name": "RTA Dubai",
-        "url": "https://www.rta.ae/wps/portal/rta/ae/home/about-rta/procurement"
+        "name": "Dubai Future Foundation",
+        "url": "https://www.dubaifuture.ae"
     },
     {
-        "name": "ADNOC Procurement",
-        "url": "https://www.adnoc.ae/en/suppliers"
+        "name": "Dubai Holding",
+        "url": "https://www.dubaiholding.com"
     },
     {
-        "name": "Dubai Culture",
-        "url": "https://dubaiculture.gov.ae"
+        "name": "Miral",
+        "url": "https://www.miral.ae"
+    },
+    {
+        "name": "Department of Culture and Tourism Abu Dhabi",
+        "url": "https://dct.gov.ae"
+    },
+    {
+        "name": "Dubai Airports",
+        "url": "https://www.dubaiairports.ae"
+    },
+    {
+        "name": "Mubadala",
+        "url": "https://www.mubadala.com"
+    },
+    {
+        "name": "Expo Centre Sharjah",
+        "url": "https://www.expocentre.ae"
+    },
+    {
+        "name": "Sharjah Museums Authority",
+        "url": "https://www.sharjahmuseums.ae"
+    },
+    {
+        "name": "Ajman Government",
+        "url": "https://www.ajman.ae"
+    },
+    {
+        "name": "RAK Government",
+        "url": "https://www.rak.ae"
+    },
+    {
+        "name": "Fujairah Government",
+        "url": "https://www.fujairah.ae"
     }
 ]
 
